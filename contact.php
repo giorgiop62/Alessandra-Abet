b6e6f7167f294ccd06ea8a53dcc1ab45d4ea2a32
Giorgio Perez <giorgiop62@gmail.com>
2024-06-06 22:36:38 +0200

    senza contact

--- contact.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$isSend = mail("g.muntoni.cs@gmail.com", $_POST['subject'], $_POST['message'], array('From' => $_POST['email']));
-
-if ($isSend) {
-    echo "OK";
-} else {
-    var_dump(error_get_last()['message']);
-}
